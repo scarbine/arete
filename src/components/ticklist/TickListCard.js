ddbaf537bb8069c0649646106af89f949ce5242a
Sam Carbine <scarbine@me.com>
2021-07-27 18:49:37 -0500

    added tick list to routes

--- src/components/ticklist/TickListCard.js
+++ src/components/ticklist/TickListCard.js
@@ -1,17 +1,28 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 import "./Ticks.css"
 
 
 export const TickListCard = ({tick}) => {
 
+	const{climberId} = useParams()
+
 	console.log()
 
-	return(
+	return( 
+
+
 		<>
+		{climberId ? (
 		<div className="tick_list_details">
 		<h4>{tick.route.routeName}</h4>
 		<h4>{tick.dateCompleted}</h4>
-		</div>
+		</div> ) : (
+		<div className="tick_list_details">
+		<h4>{tick.climber.firstName} {tick.climber.lastName}</h4>
+		<h4>{tick.dateCompleted}</h4>
+		</div>	
+		)}
 		</>
 	)
 }
