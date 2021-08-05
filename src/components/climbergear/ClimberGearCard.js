ee176c085f7d07a4b216654a0952c84f14632db7
Sam Carbine <scarbine@me.com>
2021-08-04 20:32:08 -0500

    changes

--- src/components/climbergear/ClimberGearCard.js
+++ src/components/climbergear/ClimberGearCard.js
@@ -3,6 +3,7 @@ import { ClimberGearContext } from "./ClimberGearProvider";
 import "./ClimberGear.css";
 import { useHistory } from "react-router-dom";
 import { render } from "@testing-library/react";
+import Swal from "sweetalert2";
 
 export const ClimberGearCard = ({ climbGear }) => {
   const currentUser = parseInt(localStorage.getItem("arete_customer"))
@@ -18,11 +19,29 @@ const history = useHistory()
      
   // }
 
+  const handleGearOnClick = () => {
+    Swal.fire({
+      title: `<strong> <u>${climbGear?.gear.name}</u></strong>`,
+      html:
+       `<img src="${climbGear?.gear.image}" alt="${climbGear?.gear.name}" </div>
+       <div> ${climbGear?.gear.description} </div>`,
+      // showCloseButton: true,
+      // showCancelButton: true,
+      focusConfirm: false,
+      // confirmButtonText:
+      //   '<i class="Close"></i>',
+      // confirmButtonAriaLabel: 'Close',
+      // cancelButtonText:
+      //   '<i class="fa fa-thumbs-down"></i>',
+      // cancelButtonAriaLabel: 'Thumbs down'
+    })
+  }
+
   return (
     <section className="gear_card item">
-      <img src={climbGear?.gear.image} alt={climbGear?.gear.name} />
-      <h3 className="climbGear_name">{climbGear?.gear.name}</h3>
-      <h4 className="climbGear_date_Acquired">{climbGear?.gear.dateAcquired}</h4>
+      <img onClick={handleGearOnClick} src={climbGear?.gear.image} alt={climbGear?.gear.name} />
+      <h3 onClick={handleGearOnClick} className="climbGear_name">{climbGear?.gear.name}</h3>
+      {/* <h4 className="climbGear_date_Acquired">{climbGear?.gear.dateAcquired}</h4> */}
     {(currentUser === climbGear.climberId) ? (<button onClick={handleDeleteGear}className="btn remove_gear_btn" >Remove</button>): (<></>)}
     </section>
   );
